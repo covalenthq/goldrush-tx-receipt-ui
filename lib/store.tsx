ca32f4688de1aeac85bf9044e12f7cfe471074c8
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-02-08 01:08:20 +0530

    feat: init

---
+++ lib/store.tsx
@@ -0,0 +1,61 @@
+import { createContext, useState, type ReactNode } from "react"
+import { GoldRushProvider } from "@covalenthq/goldrush-kit"
+import { useTheme } from "next-themes"
+
+import { COVALENT_API_KEY } from "./utils"
+
+interface TXContextType {
+  txHash: string
+  setTxHash: Function
+  chains: any
+  setChains: Function
+  tableState: { [key: string]: boolean }
+  setTableState: Function
+  color: string
+  setColor: Function
+  setBorderRadius: Function
+  borderRadius: string
+}
+
+export const TXContext = createContext<TXContextType>({} as TXContextType)
+
+interface TXProviderProps {
+  children: ReactNode
+}
+
+export const TXProvider: React.FC<TXProviderProps> = ({ children }) => {
+  const { theme } = useTheme()
+  const [txHash, setTxHash] = useState<string>("")
+  const [chains, setChains] = useState<[]>([])
+  const [tableState, setTableState] = useState({})
+  const [color, setColor] = useState<any>("slate")
+  const [borderRadius, setBorderRadius] = useState<any>("medium")
+
+  const mode: any = theme
+
+  return (
+    <GoldRushProvider
+      apikey={COVALENT_API_KEY ? COVALENT_API_KEY : ""}
+      mode={mode}
+      color={color}
+      border_radius={borderRadius}
+    >
+      <TXContext.Provider
+        value={{
+          txHash,
+          setTxHash,
+          chains,
+          setChains,
+          tableState,
+          setTableState,
+          setColor,
+          color,
+          setBorderRadius,
+          borderRadius,
+        }}
+      >
+        {children}
+      </TXContext.Provider>
+    </GoldRushProvider>
+  )
+}
